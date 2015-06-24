1e04ce2760ff1f2ba44e6969601a39e709384be1
Nad <kastatan@hotmail.fr>
2015-06-24 12:29:03 +0200

    rajout du menu dans index

--- index.php
+++ index.php
@@ -1,15 +1,18 @@
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
+
+
 <html>
     <head>
         <meta charset="UTF-8">
         <title></title>
     </head>
     <body>
+        <ul id="menu_horizontal">
+<li><a href="accueil.php">Accueil</a></li>
+<li><a href="#">Catégories</a></li>
+<li><a href="contact.php">Nous contacter</a></li>
+<li><a href="connexion.php">Espace client</a></li>   
+</ul>
         <?php
         // put your code here
         ?>
