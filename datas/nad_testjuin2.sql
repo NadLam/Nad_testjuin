a77bc44a4f1ed19517d0e613c103c84570721159
Nad <kastatan@hotmail.fr>
2015-06-25 14:46:47 +0200

    premier essai testjuin

---
+++ datas/nad_testjuin2.sql
@@ -0,0 +1,129 @@
+-- phpMyAdmin SQL Dump
+-- version 4.1.14
+-- http://www.phpmyadmin.net
+--
+-- Client :  127.0.0.1
+-- Généré le :  Jeu 25 Juin 2015 à 13:35
+-- Version du serveur :  5.6.17
+-- Version de PHP :  5.5.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Base de données :  `nad_testjuin`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `droit`
+--
+
+CREATE TABLE IF NOT EXISTS `droit` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `lenom` varchar(45) DEFAULT NULL,
+  `laperm` smallint(100) unsigned DEFAULT '2',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `lenom_UNIQUE` (`lenom`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `photo`
+--
+
+CREATE TABLE IF NOT EXISTS `photo` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `lenom` varchar(50) DEFAULT NULL,
+  `lextension` char(3) DEFAULT NULL,
+  `lepoids` int(10) unsigned DEFAULT NULL,
+  `lalargeur` int(10) unsigned DEFAULT NULL,
+  `lahauteur` int(10) unsigned DEFAULT NULL,
+  `letitre` varchar(60) DEFAULT NULL,
+  `ladesc` varchar(500) DEFAULT NULL,
+  `affiche` smallint(100) unsigned DEFAULT '2',
+  `utilisateur_id` int(10) unsigned NOT NULL,
+  `utilisateur_id1` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `lenom_UNIQUE` (`lenom`),
+  KEY `fk_photo_utilisateur1_idx` (`utilisateur_id1`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `photo_has_rubriques`
+--
+
+CREATE TABLE IF NOT EXISTS `photo_has_rubriques` (
+  `photo_id` int(10) unsigned NOT NULL,
+  `rubrique_id` int(10) unsigned NOT NULL,
+  KEY `fk_photo_has_rubrique_rubrique1_idx` (`rubrique_id`),
+  KEY `fk_photo_has_rubrique_photo1_idx` (`photo_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `rubriques`
+--
+
+CREATE TABLE IF NOT EXISTS `rubriques` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `lintitule` varchar(60) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `utilisateur`
+--
+
+CREATE TABLE IF NOT EXISTS `utilisateur` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `lelogin` varchar(100) DEFAULT NULL,
+  `lepass` varchar(45) DEFAULT NULL,
+  `lemail` varchar(150) DEFAULT NULL,
+  `lenom` varchar(80) DEFAULT NULL,
+  `valide` tinyint(100) unsigned DEFAULT '1',
+  `droit_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `Lelogin_UNIQUE` (`lelogin`),
+  KEY `fk_utilisateur_droit1_idx` (`droit_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+--
+-- Contraintes pour les tables exportées
+--
+
+--
+-- Contraintes pour la table `photo`
+--
+ALTER TABLE `photo`
+  ADD CONSTRAINT `fk_photo_utilisateur1` FOREIGN KEY (`utilisateur_id1`) REFERENCES `utilisateur` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Contraintes pour la table `photo_has_rubriques`
+--
+ALTER TABLE `photo_has_rubriques`
+  ADD CONSTRAINT `fk_photo_has_rubrique_photo1` FOREIGN KEY (`photo_id`) REFERENCES `photo` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_photo_has_rubrique_rubrique1` FOREIGN KEY (`rubrique_id`) REFERENCES `rubriques` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Contraintes pour la table `utilisateur`
+--
+ALTER TABLE `utilisateur`
+  ADD CONSTRAINT `fk_utilisateur_droit1` FOREIGN KEY (`droit_id`) REFERENCES `droit` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- index.php
+++ index.php
@@ -1,21 +1,132 @@
+ <?php
+
+
+require_once 'config.php';
+require_once 'connect.php';
+require_once 'fonctions.php';
+
+// si tentative de connexion
+if (isset($_POST['lelogin'])) {
+    $lelogin = traite_chaine($_POST['lelogin']);
+    $lemdp = traite_chaine($_POST['lemdp']);
+
+    // vérification de l'utilisateur dans la db
+    $sql = "SELECT  u.id, u.lemail, u.lenom, 
+		d.lenom AS nom_perm,  d.laperm 
+	FROM utilisateur u
+		INNER JOIN droit d ON u.droit_id = d.id
+    WHERE u.lelogin='$lelogin' AND u.lepass= '$lemdp';";
+    $requete = mysqli_query($mysqli, $sql)or die(mysqli_error($mysqli));
+    $recup_user = mysqli_fetch_assoc($requete);
+
+    // vérifier si on a récupèré un utilisateur
+    if (mysqli_num_rows($requete)) { // vaut true si 1 résultat (ou plus), false si 0
+
+        // si l'utilisateur est bien connecté
+
+        $_SESSION = $recup_user; // transformation des résultats de la requête en variable de session
+        $_SESSION['sid'] = session_id(); // récupération de la clef de session
+        $_SESSION['lelogin'] = $lelogin; // récupération du login (du POST après traitement)
+        // var_dump($_SESSION);
+        // redirection vers la page d'accueil (pour éviter les doubles connexions par F5)
+        header('location: ' . CHEMIN_RACINE);
+    }
+}
+
+// récupérations des images dans la table photo
+$sql = "SELECT p.lenom,p.lextension,p.letitre,p.ladesc, u.lelogin, 
+    GROUP_CONCAT(r.id) AS rubid, 
+    GROUP_CONCAT(r.lintitule SEPARATOR '~~') AS lintitule 
+    FROM photo p
+    INNER JOIN utilisateur u ON u.id = p.utilisateur_id
+    LEFT JOIN photo_has_rubriques h ON h.photo_id = p.id
+    LEFT JOIN rubriques r ON h.rubrique_id = r.id
+    GROUP BY p.id
+    ORDER BY p.id DESC; 
+    ";
+$recup_sql = mysqli_query($mysqli,$sql) or die(mysqli_error($mysqli));        
+
+?>
 <!DOCTYPE html>
-
-
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title>Accueil</title>
+        <link rel="stylesheet" href="style.css" />
+        <script src="monjs.js"></script>
     </head>
     <body>
-        <ul id="menu_horizontal">
+            <ul id="menu_horizontal">
 <li><a href="accueil.php">Accueil</a></li>
 <li><a href="#">Catégories</a></li>
+
 <li><a href="contact.php">Nous contacter</a></li>
 <li><a href="connexion.php">Espace client</a></li>   
 </ul>
-        
-       <?php
-        // put your code here
-        ?>
+        <div id="content">
+            <div id="haut"><h1>photos.be</h1> 
+                <div id="connect">
+                    <?php
+                    // si on est pas (ou plus) connecté
+                    if (!isset($_SESSION['sid']) || $_SESSION['sid'] != session_id()) {
+                        ?>
+                        <form action="" name="connection" method="POST">
+                            <input type="text" name="lelogin" required />
+                            <input type="password" name="lemdp" required />
+                            <input type="submit" value="Connexion" />
+                        </form>
+                        <a href="mdp.php">Mot de passe oublié?</a>
+                        <a href="inscription.php">Inscription</a>
+                        <?php
+                        // sinon on est connecté
+                    }else{
+                        
+                        // texte d'accueil
+                        echo "<h3>Bienvenue ".$_SESSION['u.lenom'].'</h3>';
+                        echo "<p>Vous êtes connecté en tant que <span title='".$_SESSION['d.lenom']."</span></p>";
+                        echo "<h5><a href='deconnect.php'>Déconnexion</a></h5>";
+                        
+                        // liens  suivant la permission utilisateur
+                        switch($_SESSION['laperm']){
+                            // si on est l'admin
+                            case 0 :
+                               echo "<a href='admin.php'>Administrer le site</a> - <a href='membre.php'>Espace membre</a>";
+                                break;
+                            // si on est modérateur
+                            case 1:
+                                echo "<a href='modere.php'>Modérer le site</a> - <a href='membre.php'>Espace membre</a>";
+                                break;
+                            // si autre droit (ici simple utilisateur)
+                             case 2:
+                                echo "<a href='modere.php'>Utilisateur1 du site</a> - <a href='membre.php'>Espace membre</a>";
+                                break;
+                            default :
+                                echo "<a href='membre.php'>Espace membre</a>";
+                        }
+                    }
+                    ?>
+                </div>
+            </div>
+            <div id="milieu">
+                <?php
+                // affichez les miniatures de chaques photos dans la db par id Desc, avec le titre au dessus et la description en dessous, et affichage de la grande photo dans une nouvelle fenêtre lors du clic, Bonus : afficher lelogin de l'auteur de l'image
+               while($ligne = mysqli_fetch_assoc($recup_sql)){
+                 echo "<div class='miniatures'>";
+                 echo "<h4>".$ligne['letitre']."</h4>";
+                 echo "<a href='".CHEMIN_RACINE.$dossier_gd.$ligne['lenom'].".jpg' target='_blank'><img src='".CHEMIN_RACINE.$dossier_mini.$ligne['lenom'].".jpg' alt='' /></a><br/>";
+                 $explose_rub = explode('~~',$ligne['lintitule']);
+                 $explose_id = explode(',',$ligne['rubid']);
+                 foreach($explose_rub AS $clef => $valeur){
+                     echo "<a href='?section=".$explose_id[$clef]."'>";
+                     echo $valeur."</a><br/>";
+                 }
+                 echo "<p>".$ligne['ladesc']."<br /> par <strong>".$ligne['lelogin']."</strong></p>";
+                 echo "</div>";
+               }                
+               ?> 
+            </div>
+            <div id="bas"></div>
+        </div>
     </body>
 </html>
+
